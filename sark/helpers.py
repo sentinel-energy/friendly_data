a451c800dd64e62886377099db29ce59e25a35ef
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-02-26 01:10:03 +0100

    sark/helpers.py: helper to flatten a sequence

--- sark/helpers.py
+++ sark/helpers.py
@@ -1,7 +1,18 @@
 """Helpers"""
 
+from collections import Sequence
 from importlib import import_module
+from typing import Iterable
 
 
 def import_from(module: str, name: str):
     return getattr(import_module(module), name)
+
+
+def flatten_list(lst: Iterable) -> Iterable:
+    """Flatten an arbitrarily nested list (returns a generator)"""
+    for el in lst:
+        if isinstance(el, Sequence) and not isinstance(el, (str, bytes)):
+            yield from flatten_list(el)
+        else:
+            yield el
