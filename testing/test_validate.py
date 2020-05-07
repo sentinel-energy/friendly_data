9cbe65fc20509b60f1532c2c1b4997f2a86c2ecc
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-05-07 16:59:18 +0200

    testing/test_validate.py: update fixtures, split into subtests

--- testing/test_validate.py
+++ testing/test_validate.py
@@ -3,20 +3,29 @@ from collections import defaultdict
 from glom import glom, Iter
 import pytest
 
+from sark.dpkg import create_pkg
 from sark.helpers import flatten_list
+from sark.metatools import get_license
 from sark.validate import check_pkg
 
 
-def test_check_pkg(pkg):
+def test_check_pkg(pkgdir, subtests):
+    datadir = pkgdir / "data"
+    pkg_meta = {"name": "test", "licenses": get_license("CC0-1.0")}
+    pkg = create_pkg(pkg_meta, datadir.glob("*.csv"))
+
     reports = check_pkg(pkg.descriptor)
 
-    # only errors in the bad files
-    assert all(
-        map(
-            lambda fp: "bad" in fp,
-            glom(glom(reports, [("tables", ["source"])]), Iter().flatten()),
+    with subtests.test(msg="detect bad files"):
+        # only errors in the bad files
+        assert all(
+            map(
+                lambda fp: "bad" in fp,
+                glom(
+                    glom(reports, [("tables", ["source"])]), Iter().flatten()
+                ),
+            )
         )
-    )
 
     counter = defaultdict(int)
 
@@ -27,12 +36,16 @@ def test_check_pkg(pkg):
             err["column-number"],
         )
 
-    err_row_cols = tuple(
-        flatten_list(glom(reports, [("tables", [("errors", [_proc])])]))
-    )
+    with subtests.test(msg="type/format error"):
+        err_row_cols = tuple(
+            flatten_list(glom(reports, [("tables", [("errors", [_proc])])]))
+        )
 
-    # incorrect type: (99, 2) int > float, and (101, 3) bool -> string
-    assert counter["type-or-format-error"] == 2
+        # incorrect type: (99, 2) int > float, and (101, 3) bool -> string
+        assert counter["type-or-format-error"] == 2
+
+    with subtests.test(msg="missing values"):
+        pass
 
     # FIXME: not clear why missing value detection fails
     if 0 == counter["missing-value"]:
