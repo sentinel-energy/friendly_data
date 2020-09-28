44387bfd5a0a3f18536f8515a735866e302affaf
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-09-28 23:46:31 +0200

    testing/test_validate.py: fix validation tests
    - remove subtests
    - clearly separate out type/format error, NA, and missing value errors

--- testing/test_validate.py
+++ testing/test_validate.py
@@ -1,61 +1,68 @@
 from collections import defaultdict
 from copy import deepcopy
+from functools import partial
 import json
 
-from glom import glom, Iter
-import pytest
+from glom import Assign, Check, glom, Iter, T, SKIP
+import pytest  # noqa: F401
 
 from sark.dpkg import create_pkg
 from sark.helpers import flatten_list, consume
 from sark.metatools import get_license
 from sark.validate import check_pkg, check_schema
 
+select = partial(Check, default=SKIP)
 
-def test_check_pkg(pkgdir, subtests):
+
+def test_check_pkg(pkgdir):
     datadir = pkgdir / "data"
     pkg_meta = {"name": "test", "licenses": get_license("CC0-1.0")}
     pkg = create_pkg(pkg_meta, datadir.glob("*.csv"))
+    # mark column VBN as required in sample-bad.csv
+    glom(
+        pkg.descriptor,
+        (
+            "resources",
+            Iter().filter(select(T["name"], equal_to="sample-bad")).first(),
+            "schema.fields",
+            Iter().filter(select(T["name"], equal_to="VBN")).first(),
+            Assign("constraints", {"required": True}),
+        ),
+    )
+    pkg.commit()
 
     reports = check_pkg(pkg.descriptor)
 
-    with subtests.test(msg="detect bad files"):
-        # only errors in the bad files
-        assert all(
-            map(
-                lambda fp: "bad" in fp,
-                glom(
-                    glom(reports, [("tables", ["source"])]), Iter().flatten()
-                ),
-            )
+    # only errors in files w/ bad data
+    assert all(
+        map(
+            lambda fp: "bad" in fp,
+            glom(reports, ([("tables", ["source"])], Iter().flatten())),
         )
+    )
 
     counter = defaultdict(int)
 
     def _proc(err):
         counter[err["code"]] += 1
-        return (
-            err["row-number"],
-            err["column-number"],
-        )
+        return err["row-number"], err["column-number"]
 
-    with subtests.test(msg="type/format error"):
-        err_row_cols = tuple(
-            flatten_list(glom(reports, [("tables", [("errors", [_proc])])]))
-        )
+    # extract cell numbers with errors
+    err_row_cols = tuple(
+        flatten_list(glom(reports, [("tables", [("errors", [_proc])])]))
+    )
 
-        # incorrect type: (99, 2) int > float, and (101, 3) bool -> string
-        assert counter["type-or-format-error"] == 2
+    # incorrect type: (99, 2) int -> float, and (101, 3) bool -> string
+    assert counter["type-or-format-error"] == 2
 
-    with subtests.test(msg="missing values"):
-        pass
+    # required but absent (NA): (11, 9)
+    assert counter["required-constraint"] == 1
 
-    # FIXME: not clear why missing value detection fails
-    if 0 == counter["missing-value"]:
-        pytest.xfail("cannot detect missing-value, not clear why")
-
-    # missing value: (11, 9)
+    # missing value (cell missing, not just empty): (100, 10)
     assert counter["missing-value"] == 1
-    assert (11, 9, 99, 2, 101, 3) == err_row_cols
+
+    # match cells numbers with errors
+    assert (11, 9, 99, 2, 100, 10, 101, 3) == err_row_cols
 
 
 def test_check_schema(pkgdir):
