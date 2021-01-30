240fc5465267f2969feb2617b73f5e85ffcbcadb
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-01-30 13:05:02 +0100

    datapacakge: bug report MWE

---
+++ testing/test_bugreport.py
@@ -0,0 +1,21 @@
+from datapackage import Package
+
+
+def test_pathstring(tmp_pkgdir):
+    _, dest = tmp_pkgdir
+    meta = {
+        "name": "foobarbaz",
+        "title": "Foo Bar Baz",
+        "licenses": "CC0-1.0",
+        "keywords": ["foo", "bar", "baz"],
+    }
+    pkg = Package(meta, base_path=str(dest))
+
+    fpath = "inputs/names.csv"
+    assert dest / fpath
+    pkg.infer(fpath)
+
+    res, *_ = pkg.descriptor["resources"]
+    print(res)
+    assert res["path"].count("\\") > 0
+    assert False
