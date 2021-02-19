fb61e0d8a49f4abcfcd1f31f229f5e86b56a0552
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-02-19 15:53:19 +0100

    test_registry.py: unit tests for the registry

---
+++ testing/test_registry.py
@@ -0,0 +1,46 @@
+from contextlib import nullcontext as does_not_raise
+from itertools import chain
+from pathlib import Path
+
+from pkg_resources import resource_filename
+
+import pytest
+
+import sark_registry as registry
+
+
+@pytest.mark.parametrize(
+    "col, col_t, expectation",
+    [
+        ("locs", "idxcols", does_not_raise()),
+        ("storage", "cols", does_not_raise()),
+        (
+            "notinreg",
+            "cols",
+            pytest.warns(RuntimeWarning, match="notinreg: not in registry"),
+        ),
+        (
+            "timesteps",
+            "bad_col_t",
+            pytest.raises(ValueError, match="bad_col_t: unknown column type"),
+        ),
+    ],
+)
+def test_registry(col, col_t, expectation):
+    with expectation:
+        res = registry.get(col, col_t)
+        assert isinstance(res, dict)
+        if col == "notinreg":
+            assert res == {}
+
+
+def test_getall():
+    res = registry.getall()
+    expected = ["cols", "idxcols"]
+    assert sorted(res) == expected
+    for col_t in expected:
+        curdir = Path(resource_filename("sark_registry", col_t))
+        schemas = list(
+            chain.from_iterable(curdir.glob(f"*.{fmt}") for fmt in ("json", "yaml"))
+        )
+        assert len(res[col_t]) == len(schemas)
