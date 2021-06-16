0628761a219cca0a1ebcff3e2f3434f21fcedd66
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-06-16 22:47:19 +0200

    Add missing package data (template files)

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ requirements = list(
 
 setup(
     name="friendly_data",
-    version="0.2",
+    version="0.2.1",
     description="A frictionless data package implementation for energy system data.",
     long_description=Path("README.rst").read_text(),
     long_description_content_type="text/x-rst",
     url="https://github.com/sentinel-energy/friendly_data",
     packages=find_packages(exclude=["doc", "testing", "tests", "dsts", "tmp"]),
     install_requires=requirements,
-    package_data={"friendly_data": ["py.typed"]},
+    package_data={"friendly_data": ["py.typed", "doc/*.template"]},
     entry_points={"console_scripts": ["friendly_data = friendly_data.cli:main"]},
 )
