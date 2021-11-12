ea32fa8b38e6c82c6da24ac6651c2c971747ffda
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-11-12 19:23:01 +0100

    testing/test_iamc.py: rewrite tests for IAMconv

--- testing/test_iamc.py
+++ testing/test_iamc.py
@@ -1,42 +1,53 @@
-from glom import glom, Iter
-import pyam
+import pytest
 
 from friendly_data.converters import to_df
+from friendly_data.dpkg import res_from_entry
 from friendly_data.io import dwim_file
 from friendly_data.iamc import IAMconv
 
 
 def test_iamconv(tmp_iamc):
     _, pkgdir = tmp_iamc
+    # - config: defaults for scenario & year, index names for carriers & technology
+    # - index: 1 dummy.csv entry, 3 entries w/ aggregation, 1 regular
     confpath, idxpath = pkgdir / "config.yaml", pkgdir / "index.yaml"
     conv = IAMconv.from_file(confpath, idxpath)
 
     config = dwim_file(confpath)["indices"]  # type: ignore[call-overload]
     index = dwim_file(idxpath)
     assert config.keys() == conv.indices.keys()
-    assert len(index) - 1 == len(conv.res_idx)  # one dummy entry
+    assert len(index) - 1 == len(conv.res_idx)
     assert conv.basepath == pkgdir
 
-    iamdf = pyam.IamDataFrame(pkgdir / "mini.csv")
-    exportdir = pkgdir / "outdir"
-    resources = conv.from_iamdf(iamdf, exportdir)
-    assert len(resources) == len(index) - 1
-    assert all(
-        glom(resources, Iter("path").map(lambda i: (exportdir / i).exists()).all())
-    )
+    df = conv.to_df([e["path"] for e in index])
+    assert not df.isna().any().any()
 
-    # example test with nameplate_capacity
-    cap_entry = conv.res_idx[0]
-    cap_res = resources[0]
-    assert cap_res["path"] == cap_entry["path"]
+    # check file with no aggregation
+    entry, *_ = [e for e in conv.res_idx if not e.get("agg")]
+    ref = to_df(res_from_entry(entry, pkgdir))
+    fmt = entry["iamc"]
     techs = conv.read_indices(config["technology"], pkgdir)
-    capacity = to_df(resources[0])
-    df = iamdf.as_pandas()
-    df2 = conv.to_df([cap_res["path"]], exportdir)
+    for name, title in techs.iteritems():  # result: dpkg -> iamc
+        var = fmt.format(technology=title)
+        result = df.query(f"variable == {var!r}")
+        expected = ref.query(f"technology == '{name}'")
+        assert len(expected) == len(result)
 
-    # result1: iamc -> dpkg, result2: dpkg -> iamc
-    for name, title in techs.iteritems():
-        expected = df.query(f"variable == 'Capacity|Electricity|{title}'")
-        result1 = capacity.query(f"technology == '{name}'")
-        result2 = df2.query(f"variable == 'Capacity|Electricity|{title}'")
-        assert len(expected) == len(result1) == len(result2)
+    _entry = {
+        "path": "foo.csv",
+        "idxcols": ["foo", "bar", "baz"],
+        "agg": {
+            "foo": [
+                {"values": ["xxx", "yyy", "zzz"], "variable": "bla bla"},
+                {"values": ["ppp"], "variable": "foo bar"},
+            ],
+            "bar": [{"values": ["aaa", "bbb", "ccc"], "variable": "bla bla"}],
+        },
+    }
+    with pytest.raises(AssertionError, match="one column"):
+        conv.agg_vals_all(_entry)
+
+    _entry["agg"].popitem()
+    col, vals = conv.agg_vals_all(_entry)
+    assert col == "foo"
+    assert len(vals) == 4
