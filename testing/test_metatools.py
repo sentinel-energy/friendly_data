fa0318868f14e3b964c0207af03890602ae70623
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-11-17 16:51:36 +0100

    test_metatools.py: tests for resolve_licenses

--- testing/test_metatools.py
+++ testing/test_metatools.py
@@ -1,14 +1,14 @@
 import json
+from glom.core import glom
 
 import pytest
 
-from friendly_data.metatools import (
-    ODLS,
-    get_license,
-    check_license,
-    list_licenses,
-    _fetch_license,
-)
+from friendly_data.metatools import ODLS
+from friendly_data.metatools import get_license
+from friendly_data.metatools import check_license
+from friendly_data.metatools import list_licenses
+from friendly_data.metatools import _fetch_license
+from friendly_data.metatools import resolve_licenses
 
 
 def test_fetch_license(clean_odls_cache):
@@ -48,3 +48,26 @@ def test_list_licenses(clean_odls_cache):
     lic_names = list_licenses("all")
     assert isinstance(lic_names, list)
     assert len(lic_names) > 1
+
+
+_lic_cc0 = "CC0-1.0"
+
+
+@pytest.mark.parametrize(
+    "key,val",
+    [
+        (None, None),
+        ("licenses", [_lic_cc0]),
+        ("license", [_lic_cc0]),
+        ("license", _lic_cc0),
+    ],
+)
+def test_resolve_licenses(key, val):
+    meta = {"name": "foo"}
+
+    if key:
+        meta.update({key: val})
+        res = resolve_licenses(meta)
+        assert glom(res, "licenses.0.name") == _lic_cc0
+    else:
+        assert resolve_licenses(meta) == {"name": "foo"}
