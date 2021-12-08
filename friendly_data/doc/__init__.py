4b1e1a631b74b351e566010896fb4d87975ce0e3
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-12-08 14:05:34 +0100

    friendly_data/doc: add markdown template for cli registry doc

--- friendly_data/doc/__init__.py
+++ friendly_data/doc/__init__.py
@@ -3,7 +3,10 @@ from typing import Dict
 from jinja2 import BaseLoader, Environment, FileSystemLoader
 from pkg_resources import resource_filename
 
+from friendly_data import logger_config
 import friendly_data_registry as registry
+
+logger = logger_config(fmt="{name}: {levelname}: {message}")
 
 
 def template_from_str(template: str):
@@ -16,17 +19,25 @@ def get_template(name: str):
     return env.get_template(name)
 
 
-def entry(schema: Dict, f: str) -> str:
-    return get_template("entry.rst.template").render({"file": f, **schema})
+def entry(schema: Dict, f: str, markup: str = "rst") -> str:
+    return get_template(f"entry.{markup}.template").render({"file": f, **schema})
 
 
-def page() -> str:
+def page(*, markup: str = "rst", col_t: str = "") -> str:
+    code = "``" if markup == "rst" else "`"
     col_types = {
-        "cols": "Value columns - ``cols``",
-        "idxcols": "Index columns - ``idxcols``",
+        "cols": f"Value columns - {code}cols{code}",
+        "idxcols": f"Index columns - {code}idxcols{code}",
     }
+    if col_t == "cols":
+        col_types.pop("idxcols")
+    elif col_t == "idxcols":
+        col_types.pop("cols")
+    else:
+        logger.warning(f"{col_t}: unknown column type, will return all")
     contents = [
-        (col_types[col_t], [entry(schema, f) for schema, f in schemas])
-        for col_t, schemas in registry.getall(with_file=True).items()
+        (col_types[_col_t], [entry(schema, f, markup) for schema, f in schemas])
+        for _col_t, schemas in registry.getall(with_file=True).items()
+        if _col_t in col_types
     ]
-    return get_template("page.rst.template").render({"sections": contents})
+    return get_template(f"page.{markup}.template").render({"sections": contents})
